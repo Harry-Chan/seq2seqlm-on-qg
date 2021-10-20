ef643ab7fdf05b9fa4851a24b99742cad2317862
Harry-Chan <harry831120@gmail.com>
2021-10-20 12:47:31 +0000

    update

--- setup_scorer.py
+++ setup_scorer.py
@@ -5,7 +5,7 @@ if __name__ == "__main__":
     os.system(
         "sudo apt-get -y update && sudo apt-get -y install default-jre && sudo apt-get -y install default-jdk"
     )
-    os.system("export LC_ALL=C.UTF-8&&export LANG=C.UTF-8&&nlg-eval --setup")
+    os.system("export LC_ALL=C.UTF-8&&export LANG=C.UTF-8&&nlg-eval --setup ${data_path}")
     import stanza
 
     stanza.download("en")
