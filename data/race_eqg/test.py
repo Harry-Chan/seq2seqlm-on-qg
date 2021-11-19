51fcab4bfcb2fc4ed0e1d4bf33d09d202a470c1e
Harry-Chan <harry831120@gmail.com>
2021-11-19 10:02:50 +0000

    fix(data/):fix data type

--- data/race_eqg/test.py
+++ data/race_eqg/test.py
@@ -4,8 +4,15 @@ import re
 
 from pygments.unistring import No
 
-with open("test_keywords.json") as file:
+with open("test.json") as file:
     data = json.load(file)
+
+src = ''
+for ele in data:
+    src += ele['context'] + "[SEP]" + ele["answer"] + "\n"
+
+with open('src_test.txt','w') as outfile:
+    outfile.write(src)
 
 
 # print(len(data))
@@ -33,15 +40,15 @@ with open("test_keywords.json") as file:
 #     file.write(res.strip())
 
 
-for ele in data:
-    answer = ele["answer"]
-    if answer[-1] == ".":
-        answer = answer[:-2]
-    sp_answer = answer.split(" ")
-    for i in range(len(sp_answer), 0, -1):
-        for n in nltk.ngrams(answer.split(" "), i):
-            print(n)
-        input()
+# for ele in data:
+#     answer = ele["answer"]
+#     if answer[-1] == ".":
+#         answer = answer[:-2]
+#     sp_answer = answer.split(" ")
+#     for i in range(len(sp_answer), 0, -1):
+#         for n in nltk.ngrams(answer.split(" "), i):
+#             print(n)
+#         input()
 
 
 # json.dump(data[:100], open("train_keywords_100.json", "w"))
