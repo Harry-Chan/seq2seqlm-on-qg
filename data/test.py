98ce6f16217ae25aaa5172332bc6e4a745641ad8
Harry-Chan <harry831120@gmail.com>
2021-11-22 13:45:10 +0000

    update(.gitignore):add .gitignore

--- data/test.py
+++
@@ -1,9 +0,0 @@
-import json
-
-with open('squad_nqg/train_keywords.json') as file:
-    data = json.load(file)
-
-for ele in data:
-    del ele['noun_keywords']
-
-json.dump(data,open('squad_nqg/train_keywords.json','w'))
--- nqg/sentence/README.md
+++
@@ -1,1 +0,0 @@
-# Sentence-level model
