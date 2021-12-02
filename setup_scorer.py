8ef0857599ecebd96d7032c2d23ed4769f503831
Harry-Chan <harry831120@gmail.com>
2021-12-02 16:57:04 +0000

    update(nlg): remove nlg package

--- setup_scorer.py
+++ setup_scorer.py
@@ -5,9 +5,9 @@ if __name__ == "__main__":
     os.system(
         "sudo apt-get -y update && sudo apt-get -y install default-jre && sudo apt-get -y install default-jdk"
     )
-    os.system("export LC_ALL=C.UTF-8&&export LANG=C.UTF-8&&nlg-eval --setup")
-    import stanza
+    # os.system("export LC_ALL=C.UTF-8&&export LANG=C.UTF-8&&nlg-eval --setup")
+    # import stanza
 
     # original nqg scorer run with py2, we maintain a py3 version
     # see this PR for more info: https://github.com/xinyadu/nqg/pull/44
-    os.system('git clone -b python3 https://github.com/p208p2002/nqg.git')
+    os.system("git clone -b python3 https://github.com/p208p2002/nqg.git")
